cae75171ea5810bdee3419686392a1d53ab36e21
mrx <avi_tv@yahoo.com>
2018-11-26 12:50:13 +0200

    Mr Katav

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            I WAS CHANE BY FEATURE BRANCH
+            MR KATAV
           </p>
           <a
             className="App-link"
